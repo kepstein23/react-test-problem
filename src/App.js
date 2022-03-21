60b5a3b4e4391c16e3e18d14c4dcedf75cc4720a
kepstein23 <97930010+kepstein23@users.noreply.github.com>
2022-03-20 23:00:18 -0400

    Build button

--- src/App.js
+++ src/App.js
@@ -1,20 +1,37 @@
 import React from 'react';
 
 export default class App extends React.Component {
+  
   constructor(props) {
     super(props);
-    this.state = {
-      
+    this.state = { 
+      clickMe: true,
+      buttonText: "click me"
     };
+    this.handleClickMe = this.handleClickMe.bind(this);
   }
 
+  
+
+
   //handler goes here
+  handleClickMe() {
+    if (this.state.clickMe) {
+      this.setState({buttonText: "thanks"});
+      this.setState({clickMe: false});
+    } else {
+      this.setState({buttonText: "click me"});
+      this.setState({clickMe: true});
+    }
+    console.log(this.state.clickMe);
+    console.log(this.state.buttonText);
+  }
 
   render() {
     return (
       <div>
         <p>Greetings!</p>
-        <p>Button goes here</p>
+        <button type="submit" onClick={this.handleClickMe}>{this.state.buttonText}</button>
       </div>
     );
   }
