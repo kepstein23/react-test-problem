b73553a2995e75c9ec4293d13ccff1514f9de728
kepstein23 <97930010+kepstein23@users.noreply.github.com>
2022-03-21 08:12:25 -0400

    add story and tests

--- src/App.stories.js
+++ src/App.stories.js
@@ -2,30 +2,15 @@
 // See https://storybook.js.org/docs/react/writing-stories/introduction#how-to-write-stories
 
 import React from 'react';
-
 import App from './App';
 
-// More on default export: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
 export default {
   component: App,
-  title: 'App'
 };
 
-export function clickMeText() {
+export function ClickMe() {
   return (
-    <div>
-      <p>Greetings!</p>
-      <button type="submit" onClick={() => {}}>click me</button>
-    </div>
-    
-  )
-}
-export function thanksText() {
-  return (
-    <div>
-      <p>Greetings!</p>
-      <button type="submit">thanks</button>
-    </div>
+    <App/>
     
   )
 }
--- src/App.test.js
+++ src/App.test.js
@@ -1,7 +1,24 @@
 import { render, screen, fireEvent } from '@testing-library/react';
 
 // import your story from App.stories.js
-
+import Meta, { ClickMe } from './App.stories.js'
+import { composeStory } from '@storybook/testing-react';
 
 // Add your tests here
 // See https://storybook.js.org/docs/react/writing-tests/importing-stories-in-tests#example-with-testing-library
+
+const ClickMeStory = composeStory(ClickMe, Meta);
+
+it("the button initially displays click me", () => {
+    render(<ClickMeStory/>);
+    const linkElement = screen.getByText("click me");
+    expect(linkElement).toBeInTheDocument();    
+  })
+
+  it("changes on click", () => {
+    render(<ClickMeStory/>);
+    const handleClick = jest.fn();
+    fireEvent.click(screen.getByText("click me"));
+    expect(screen.getByText("thanks")).toBeInTheDocument();
+  })
+    
